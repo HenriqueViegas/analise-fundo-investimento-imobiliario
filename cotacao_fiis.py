c32d02abf99671313901b3abf93f5f036201a037
Henrique Viegas <dagostiniviegas@gmail.com>
2026-02-08 12:23:31 -0300

    Inclusão gitgnore e exclusão de outros arquivos antigos

--- cotacao_fiis.py
+++ cotacao_fiis.py
@@ -9,151 +9,130 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
 
-# '''Apagar arquivos na pasta download'''
+'''Apagar arquivos na pasta download'''
 
-# caminho_da_pasta = os.path.join(os.getcwd(), 'downloads')
+caminho_da_pasta = os.path.join(os.getcwd(), 'downloads')
 
-# # Percorre os itens dentro do diretório downloads
-# for item in os.listdir(caminho_da_pasta):
-#     caminho_completo_arquivo = os.path.join(caminho_da_pasta, item)
-#     if os.path.isfile(caminho_completo_arquivo) and (item.endswith('.xls') or item.endswith('.xlsx') or item.endswith('.csv')):
-#         try:
-#             os.remove(caminho_completo_arquivo)
-#             print(f'*** Arquivo {item} removido com sucesso ***')
-#         except OSError as e:
-#             print(f'*** Erro ao apagar o arquivo {item}: {e.strerror} ***')
-
-
-# '''Configuração do chromedriver'''
-
-# # Pasta onde os downloads são salvos
-# diretorio_downloads = os.path.join(os.getcwd(), "downloads")
-# if not os.path.exists(diretorio_downloads):
-#     os.makedirs(diretorio_downloads)
-
-# chrome_options = webdriver.ChromeOptions()
-# prefs = {"download.default_directory": diretorio_downloads}
-# chrome_options.add_experimental_option("prefs", prefs)
+# Percorre os itens dentro do diretório downloads
+for item in os.listdir(caminho_da_pasta):
+    caminho_completo_arquivo = os.path.join(caminho_da_pasta, item)
+    if os.path.isfile(caminho_completo_arquivo) and (item.endswith('.xls') or item.endswith('.xlsx') or item.endswith('.csv')):
+        try:
+            os.remove(caminho_completo_arquivo)
+            print(f'*** Arquivo {item} removido com sucesso ***')
+        except OSError as e:
+            print(f'*** Erro ao apagar o arquivo {item}: {e.strerror} ***')
 
 
-# ''''Inicia o driver - status invest'''
-# # chrome_options.add_argument("--headless")  # Roda sem abrir a janela (mais rápido)
-# # chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# wait = WebDriverWait(driver, 15)
+'''Configuração do chromedriver'''
 
-# try:
-#     url = 'https://statusinvest.com.br/fundos-imobiliarios/busca-avancada'
-#     driver.get(url)
+# Pasta onde os downloads são salvos
+diretorio_downloads = os.path.join(os.getcwd(), "downloads")
+if not os.path.exists(diretorio_downloads):
+    os.makedirs(diretorio_downloads)
 
-#     # Clicar no botão "BUSCAR" - Utilizado XPath
-#     btn_buscar = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[3]/div/div/div/button[2]')))
-#     btn_buscar.click()
-#     print("*** Busca realizada... ***")
-
-#     # Clicar no botão "DOWNLOAD" - Utilizado XPath
-#     btn_download = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[4]/div/div[1]/div[2]/a')))
-#     btn_download.click()
-#     print(f"*** Download iniciado! O arquivo será salvo em: {caminho_da_pasta} ***")
-
-#     # Pequena espera apenas para garantir que o download comece antes de fechar
-#     import time
-#     time.sleep(3)
-
-# finally:
-#     driver.quit()
+chrome_options = webdriver.ChromeOptions()
+prefs = {"download.default_directory": diretorio_downloads}
+chrome_options.add_experimental_option("prefs", prefs)
 
 
-# ''''Leitura e tratamento do arquivo baixado'''
-# arquivo_csv = glob.glob(os.path.join(caminho_da_pasta, '*.csv'))
-
-# if not arquivo_csv:
-#   print('*** Nenhum arquivo encontrado na pasta ***')
-# else:
-#   if len(arquivo_csv) > 1:
-#           print(f"*** Atenção: há {len(arquivo_csv)} arquivos CSV na pasta. Usando o primeiro encontrado. ***")
-
-#   arquivo_csv = arquivo_csv[0]
-#   print(f"*** Tentando ler o arquivo: {arquivo_csv} ***")
-
-#   try:
-#       df = pd.read_csv(arquivo_csv, sep=";", encoding="cp1252")
-#       print("*** Arquivo lido com sucesso! ***")
-      
-      
-#       df = df[['TICKER', 'PRECO', 'ULTIMO DIVIDENDO', 'DY', 'VALOR PATRIMONIAL COTA', 'P/VP', 'PATRIMONIO', 'PERCENTUAL EM CAIXA', 'N COTISTAS']]
-
-#   except Exception as e:
-#       print(f"*** Erro ao ler o arquivo {arquivo_csv}: {e} ***")
-
-
-
-# ''''Informações necessárias:
-
-#     - taxa de performance
-#     - taxa de adm
-#     - vacancia
-#     - concentração em um mesmo ativo
-#     - vencimento dos contratos
-#     - tipo: tijolo/papel ok
-#     - cnpj ok
-#     - administrador ok 
-# '''
-
-
-
-
-
-# '''Captura infos do site de FIIS - complemento de informações com CNPJ, Segmento e administrador'''
-# ''''Inicia o driver'''
-
+''''Inicia o driver - status invest'''
 # chrome_options.add_argument("--headless")  # Roda sem abrir a janela (mais rápido)
 # chrome_options.add_argument("--no-sandbox")
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-# wait = WebDriverWait(driver, 5)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+wait = WebDriverWait(driver, 15)
+
+try:
+    url = 'https://statusinvest.com.br/fundos-imobiliarios/busca-avancada'
+    driver.get(url)
+
+    # Clicar no botão "BUSCAR" - Utilizado XPath
+    btn_buscar = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[3]/div/div/div/button[2]')))
+    btn_buscar.click()
+    print("*** Busca realizada... ***")
+
+    # Clicar no botão "DOWNLOAD" - Utilizado XPath
+    btn_download = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-2"]/div[4]/div/div[1]/div[2]/a')))
+    btn_download.click()
+    print(f"*** Download iniciado! O arquivo será salvo em: {caminho_da_pasta} ***")
+
+    # Pequena espera apenas para garantir que o download comece antes de fechar
+    import time
+    time.sleep(3)
+
+finally:
+    driver.quit()
 
 
-# '''Captura os dados principais dos FIIs'''
-# dados_finais = []
+''''Leitura e tratamento do arquivo baixado'''
+arquivo_csv = glob.glob(os.path.join(caminho_da_pasta, '*.csv'))
 
-# try:
-#     print("Acessando a lista de fundos...")
-#     url_base = 'https://fiis.com.br/'
-#     driver.get(url_base + 'lista-de-fundos-imobiliarios/')
+if not arquivo_csv:
+  print('*** Nenhum arquivo encontrado na pasta ***')
+else:
+  if len(arquivo_csv) > 1:
+          print(f"*** Atenção: há {len(arquivo_csv)} arquivos CSV na pasta. Usando o primeiro encontrado. ***")
 
-#     # Exemplo com os 5 primeiros para teste
-#     for fundo in df['TICKER']:
-#         try:
-#             print(f"Coletando: {fundo}...")
-#             if "Página não encontrada" in driver.page_source:
-#                 print(f"Página inválida para {fundo}")
-#                 continue
+  arquivo_csv = arquivo_csv[0]
+  print(f"*** Tentando ler o arquivo: {arquivo_csv} ***")
 
-#             driver.get(url_base + fundo.lower() + '/')
-#             cnpj = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[1]/b'))).text
-#             segmento = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[7]/b'))).text 
-#             adm = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'informations__adm__name'))).text
-#             dados_finais.append({
-#                 'TICKER': fundo,
-#                 'SEGMENTO': segmento,
-#                 'CNPJ': cnpj,
-#                 'ADMINISTRADOR': adm
-#             })    
+  try:
+      df = pd.read_csv(arquivo_csv, sep=";", encoding="cp1252")
+      print("*** Arquivo lido com sucesso! ***")
+      
+      
+      df = df[['TICKER', 'PRECO', 'ULTIMO DIVIDENDO', 'DY', 'VALOR PATRIMONIAL COTA', 'P/VP', 'PATRIMONIO', 'PERCENTUAL EM CAIXA', 'N COTISTAS']]
 
-#         except Exception as e:
-#             print(f"Erro ao processar {fundo}: {e}")
-#             continue  
-# finally:
-#     driver.quit()   
-# df_complementar = pd.DataFrame(dados_finais)
+  except Exception as e:
+      print(f"*** Erro ao ler o arquivo {arquivo_csv}: {e} ***")
 
-# df_final = pd.merge(df, df_complementar, on="TICKER")
-# df_final.to_excel(caminho_da_pasta + '_analise_completa_fii.xlsx', index=False)
 
-'''APAGAR APÓS OS TESTES'''
-caminho_da_pasta = os.path.join(os.getcwd(), 'downloads')
-df_final = pd.read_excel("C:\\Users\\dagos\\OneDrive\\Área de Trabalho\\Cursos\\Projetos_python\\projeto_fiis\\downloads_analise_completa_fii.xlsx")
-'''APAGAR APÓS OS TESTES'''
+
+'''Captura infos do site de FIIS - complemento de informações com CNPJ, Segmento e administrador'''
+''''Inicia o driver'''
+
+chrome_options.add_argument("--headless")  # Roda sem abrir a janela (mais rápido)
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+wait = WebDriverWait(driver, 5)
+
+
+'''Captura os dados principais dos FIIs'''
+dados_finais = []
+
+try:
+    print("Acessando a lista de fundos...")
+    url_base = 'https://fiis.com.br/'
+    driver.get(url_base + 'lista-de-fundos-imobiliarios/')
+
+    # Exemplo com os 5 primeiros para teste
+    for fundo in df['TICKER']:
+        try:
+            print(f"Coletando: {fundo}...")
+            if "Página não encontrada" in driver.page_source:
+                print(f"Página inválida para {fundo}")
+                continue
+
+            driver.get(url_base + fundo.lower() + '/')
+            cnpj = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[1]/b'))).text
+            segmento = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[7]/b'))).text 
+            adm = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'informations__adm__name'))).text
+            dados_finais.append({
+                'TICKER': fundo,
+                'SEGMENTO': segmento,
+                'CNPJ': cnpj,
+                'ADMINISTRADOR': adm
+            })    
+
+        except Exception as e:
+            print(f"Erro ao processar {fundo}: {e}")
+            continue  
+finally:
+    driver.quit()   
+df_complementar = pd.DataFrame(dados_finais)
+
+df_final = pd.merge(df, df_complementar, on="TICKER")
+df_final.to_excel(caminho_da_pasta + '_analise_completa_fii.xlsx', index=False)
 
 '''Conversão e limpeza de dados'''
 df_final = df_final[~df_final["N COTISTAS"].isnull()]
@@ -185,69 +164,3 @@ df_final.to_excel(caminho_da_pasta + '_filtro_fundos_elegiveis.xlsx', index=Fals
 
 # aprimorar o merge
 # identificar quais fundos possuem os melhores parâmetros
-
-# criar uma lista com esses fundos ou um novo dataframe
-# ler o arquivo de relatório gerencial para obter mais infos (?)
-# Criar um catalogo com os melhores fundos e os riscos envolvidos
-
-
-
-
-#             rendimento = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_header-2"]/div/div/div[1]/div[2]/div/div[2]/p[1]/b'))).text
-#             rendimento = float(rendimento.replace('%', '').replace(',', '.').strip())
-
-#             dy = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_dividends-2"]/div[2]/div[2]/div/div/div/div/div[2]/div[1]/div[5]'))).text
-#             dy = float(dy.replace('%', '').replace(',', '.').strip()) 
-
-#             pvp = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_header-2"]/div/div/div[1]/div[2]/div/div[4]/p[1]/b'))).text
-#             pvp = float(pvp.replace('%', '').replace(',', '.').strip()) 
-
-#             preco = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_quotations_chart-2"]/div[1]/div[2]/div[1]/div[1]/span[2]'))).text
-#             preco = float(preco.replace('%', '').replace(',', '.').strip()) 
-
-#             vlr_patrimonial_cota = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_quotations_chart-2"]/div[2]/div[3]/p[1]/b'))).text
-#             vlr_patrimonial_cota = float(vlr_patrimonial_cota.replace('%', '').replace(',', '.').strip()) 
-
-#             patrimonio_liquido = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[4]/b'))).text
-#             patrimonio_liquido = float(patrimonio_liquido.replace('R$', '').replace(',', '.').replace('.', '').strip()) 
-
-#             segmento = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[7]/b'))).text 
-
-#             cnpj = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_informations-2"]/div[3]/p[1]/b'))).text
-
-#             adm = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'informations__adm__name'))).text
-
-#             vlr_caixa = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="carbon_fields_fiis_quotations_chart-2"]/div[2]/div[1]/p[1]/b'))).text
-
-#             dados_finais.append({
-#                 'Ticker': fundo,
-#                 'Dividend_Yield(%)': dy,
-#                 'Rendimento': rendimento,
-#                 'Pvp': pvp,
-#                 'Preco': preco,
-#                 'Vlr_Patrimonial_Cota': vlr_patrimonial_cota,
-#                 'Patrimonio_Liquido': patrimonio_liquido,
-#                 'Segmento': segmento,
-#                 'CNPJ': cnpj,
-#                 'Administrador': adm,
-#                 'Vlr_Caixa': vlr_caixa
-#             })
-
-#         except Exception as e:
-#             print(f"Erro ao processar {fundo}: {e}")
-#             continue
-
-# finally:
-#     driver.quit()
-
-# '''Salva o dataframe'''
-# nome_arquivo = "resultado_fiis.xlsx"
-
-# # 2. Junte a pasta com o nome do arquivo
-# caminho_final = os.path.join(diretorio_downloads, nome_arquivo)
-# df = pd.DataFrame(dados_finais)
-# df = df.reindex(columns = ['Ticker', 'CNPJ', 'Preco', 'Vlr_Patrimonial_Cota', 'Pvp', 'Rendimento', 'Dividend_Yield(%)', 'Patrimonio_Liquido', 'Vlr_Caixa', 'Administrador'])
-# df.to_excel(caminho_final, index=False)
-# print('Dataframe gerado com sucesso')
-
-
